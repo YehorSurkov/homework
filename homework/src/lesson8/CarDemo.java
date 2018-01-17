c841abc6596b3ab70e7c7b1e422774e483e58eb8
YehorSurkov <gor.surkov@gmail.com>
2018-01-17 12:39:47 +0200

    Pffff

--- homework/src/lesson8/CarDemo.java
+++ homework/src/lesson8/CarDemo.java
@@ -10,11 +10,20 @@ public class CarDemo {
     public static void main(String[] args) {
         Engine eng1 = new Engine(30.5, "Audi LTD");
         Engine eng2 = new Engine(50.8, "Porsche mnf");
-        Engine eng3 = new Engine(45.5, "UkrAuto");
+        Engine eng3 = new Engine(45.5, "UA");
         Driver drv = new Driver("Yehor", "Surkov", 23, 0);
         Car car = new Car("audi", "L", 1300, drv, eng1);
         Lorry lorry = new Lorry("KraZ", "Ukr Gruzovik", 20000, drv, eng3);
         SportCar sportC = new SportCar("Porsche", "S", 600, drv, eng2);
+
+        car.printInfo();
+        System.out.println();
+        lorry.printInfo();
+        System.out.println();
+        sportC.printInfo();
+
+        eng2.setPower(66.6);
+        eng3.setManufacturer("UkrAuto");
 
         car.printInfo();
         System.out.println();
