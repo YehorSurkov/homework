8749c11a8f12a64c6b6e53c1b8f3fe57a5d2a77e
YehorSurkov <yehor.surkov@gmail.com>
2018-01-21 13:53:06 +0200

    Pffff

--- homework/src/lesson7/MCalculator.java
+++ homework/src/lesson7/MCalculator.java
@@ -3,53 +3,63 @@ package lesson7;
 public class MCalculator {
     public int callCount;
     public int rawCount;
-    public int[][] matrix;
+    public double[][] matrix;
 
-    public MCalculator(int[][] mat) {
+    public MCalculator(double[][] mat) {
         rawCount = mat.length;
         callCount = mat[0].length;
         matrix = mat;
     }
 
-    public int[][] addMatrixes(int[][] m) {
-        if ((rawCount == m.length) && (callCount == m[0].length)) {
-            int[][] r = new int[rawCount][callCount];
+    public MCalculator addMatrixes(MCalculator m) {
+        if ((rawCount == m.matrix.length) && (callCount == m.matrix[0].length)) {
+            MCalculator result = new MCalculator(new double[rawCount][callCount]);
             for (int i = 0; i < rawCount; i++) {
                 for (int j = 0; j < matrix[i].length; j++) {
-                    r[i][j] = matrix[i][j] + m[i][j];
+                    result.matrix[i][j] = matrix[i][j] + m.matrix[i][j];
                 }
             }
-            return r;
+            return result;
         } else {
             System.out.println("Wrong size");
-            return new int[1][1];
+            return new MCalculator(new double[1][1]);
         }
     }
 
-    public int[][] multToNumber(int alpha) {
-        int[][] r = new int[rawCount][callCount];
+    public MCalculator multToNumber(double alpha) {
+        MCalculator result = new MCalculator(new double[rawCount][callCount]);
         for (int i = 0; i < rawCount; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                r[i][j] = alpha * matrix[i][j];
+                result.matrix[i][j] = matrix[i][j] * alpha;
             }
         }
-        return r;
+        return this;
     }
 
-    public int[][] multMatrixRight(int[][] m) {
-        if (callCount == m.length) {
-            int[][] r = new int[rawCount][m[0].length];
+    public MCalculator multMatrixRight(MCalculator m) {
+        if (callCount == m.matrix.length) {
+            MCalculator r = new MCalculator(new double[rawCount][m.matrix[0].length]);
             for (int i = 0; i < rawCount; i++) {
-                for (int j = 0; j < m[0].length; j++) {
-                    r[i][j] = 0;
+                for (int j = 0; j < m.matrix[0].length; j++) {
+                    r.matrix[i][j] = 0;
                     for (int cntr = 0; cntr < callCount; cntr++)
-                        r[i][j] = r[i][j] + matrix[i][cntr] * m[cntr][j];
+                        r.matrix[i][j] += matrix[i][cntr] * m.matrix[cntr][j];
                 }
             }
             return r;
         } else {
             System.out.println("Wrong size");
-            return new int[1][1];
+            return new MCalculator(new double[1][1]);
         }
+    }
+
+    public static void showResult(MCalculator m) {
+        for (double[] raw : m.matrix) {
+            for (double el : raw) {
+                System.out.print(el + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
     }
 }
--- homework/src/lesson7/MCalculatorDemo.java
+++ homework/src/lesson7/MCalculatorDemo.java
@@ -3,31 +3,21 @@ package lesson7;
 public class MCalculatorDemo {
     public static void main(String[] args) {
 
-        MCalculator arr1 = new MCalculator(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        MCalculator arr1 = new MCalculator(new double[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
 
         int alpha1 = 2;
-        MCalculator arr2 = new MCalculator(new int[][]{{1, 2}, {4, 5}, {7, 8}});
-        MCalculator arr3 = new MCalculator(new int[][]{{1, 2, 3}, {4, 5, 6}});
-        MCalculator arr4 = new MCalculator(new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+        MCalculator arr2 = new MCalculator(new double[][]{{1, 2}, {4, 5}, {7, 8}});
+        MCalculator arr3 = new MCalculator(new double[][]{{1, 2, 3}, {4, 5, 6}});
+        MCalculator arr4 = new MCalculator(new double[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
 
-        System.out.println("My matrix consists " + arr1.rawCount + "raws");
+        System.out.println("My matrix consists " + arr1.rawCount + "raws\n");
 
-        showResult(arr1.matrix);
-        showResult(arr1.addMatrixes(arr4.matrix));
-        showResult(arr1.addMatrixes(arr2.matrix));
-        showResult(arr1.multToNumber(alpha1));
-        showResult(arr1.multMatrixRight(arr2.matrix));
-        showResult(arr1.multMatrixRight(arr3.matrix));
-        showResult(arr1.multMatrixRight(arr4.matrix));
-    }
-
-    public static void showResult(int[][] res) {
-        for (int i = 0; i < res.length; i++) {
-            for (int j = 0; j < res[i].length; j++) {
-                System.out.print(res[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("\n");
+        MCalculator.showResult(arr1);
+        MCalculator.showResult(arr1.addMatrixes(arr4));
+        MCalculator.showResult(arr1.addMatrixes(arr2));
+        MCalculator.showResult(arr1.multToNumber(alpha1));
+        MCalculator.showResult(arr1.multMatrixRight(arr2));
+        MCalculator.showResult(arr1.multMatrixRight(arr3));
+        MCalculator.showResult(arr1.multMatrixRight(arr4));
     }
 }
