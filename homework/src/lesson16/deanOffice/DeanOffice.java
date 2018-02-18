acbe7f3c7a8e3877f197d9f51295efe5c7b3e46c
YehorSurkov <yehor.surkov@gmail.com>
2018-02-18 16:24:16 +0200

    IIII

--- homework/src/lesson16/deanOffice/DeanOffice.java
+++ homework/src/lesson16/deanOffice/DeanOffice.java
@@ -1,4 +1,53 @@
 package lesson16.deanOffice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class DeanOffice {
+    public static void main(String[] args) {
+        List<Student> uni = new ArrayList<>();
+        uni.add(new Student("Yehor Surkov", "TL-54", 4, 7));
+        uni.add(new Student("Ihor D", "TL-54", 4, 7));
+        uni.add(new Student("NL", "TP-54", 3, 10));
+        uni.add(new Student("LK", "TP-54", 3, 10));
+        uni.add(new Student("AM", "TYA-54", 5, 5));
+        uni.add(new Student("S1", "TG-54", 4, 12));
+        uni.add(new Student("S2", "TG-54", 3, 17));
+        uni.add(new Student("S3", "TH-54", 1, 12));
+        uni.add(new Student("S4", "TH-54", 4, 19));
+
+        whoIsRemain(uni);
+        throwUni(uni, "TL-54");
+    }
+
+    public static List<Student> whoIsRemain(List<Student> uni) {
+        ListIterator<Student> it = uni.listIterator();
+        List<Student> success = new ArrayList<>();
+        Student st;
+        while (it.hasNext()) {
+            st = it.next();
+            if (st.gpa() > 2.5) {
+                success.add(st);
+                System.out.println(st.getFullName() + ", we have a grate opportunity to inform you... Our congrats!:) Your GPA is " + st.gpa());
+            } else {
+                System.out.println(st.getFullName() + ", ti is a stupid creature, nafig s plyagha! Your GPA is " + st.gpa());
+            }
+        }
+        return success;
+    }
+
+    public static List<Student> throwUni(List<Student> uni, String group) {
+        ListIterator<Student> it = uni.listIterator();
+        List<Student> potok = new ArrayList<>();
+        Student st;
+        while (it.hasNext()) {
+            st = it.next();
+            if (st.getGroup().equals(group)) {
+                potok.add(st);
+                System.out.println(st.getFullName() + " " + st.getGroup());
+            }
+        }
+        return potok;
+    }
 }
--- homework/src/lesson16/deanOffice/Student.java
+++ homework/src/lesson16/deanOffice/Student.java
@@ -1,4 +1,93 @@
 package lesson16.deanOffice;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Student {
+    private String fullName;
+    private String group;
+    private int course;
+    private int[] marks;
+
+    public Student() {
+
+    }
+
+    public Student(String fullName, String group, int course, int numberOfMarks) {
+        this.fullName = fullName;
+        this.group = group;
+        this.course = course;
+        marks = new int[numberOfMarks];
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = (int) Math.round(5 * Math.random());
+        }
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public int[] getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int[] marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return course == student.course &&
+                Objects.equals(fullName, student.fullName) &&
+                Objects.equals(group, student.group) &&
+                Arrays.equals(marks, student.marks);
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = Objects.hash(fullName, group, course);
+        result = 31 * result + Arrays.hashCode(marks);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "fullName='" + fullName + '\'' +
+                ", group='" + group + '\'' +
+                ", course=" + course +
+                '}';
+    }
+
+    public double gpa() {
+        double gPA = 0;
+        for (int mark : marks) {
+            gPA += mark;
+        }
+        return gPA / marks.length;
+    }
 }
